a7c8bf8423defeda1060185f988622bd8a497102
tolkachevART <168184655+tolkachevART@users.noreply.github.com>
2024-11-24 07:29:01 +0300

    CORS settings and documentations

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,8 @@ INSTALLED_APPS = [
     'rest_framework',
     'django_filters',
     'rest_framework_simplejwt',
-
+    'drf_yasg',
+    'corsheaders',
 
     'habits',
     'users',
@@ -35,6 +36,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -148,3 +150,16 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TELEGRAM_URL = 'https://api.telegram.org/bot'
 
+# CORS settings
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3030",
+    "http://another.domain.com"
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^http://localhost:3030$",
+    r"^http://another.domain.com$"
+]
+
+CORS_ALLOW_CREDENTIALS = False
