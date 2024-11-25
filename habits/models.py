def6461e4a1e52eacd24416d8fdd8caca44b106a
tolkachevART <168184655+tolkachevART@users.noreply.github.com>
2024-11-25 11:23:03 +0300

    remarks

--- habits/models.py
+++ habits/models.py
@@ -1,7 +1,11 @@
 from django.db import models
 
-from nullable import NULLABLE
 from users.models import User
+
+PERIOD_CHOICES = (
+    (True, 'Ежедневная'),
+    (False, 'Еженедельная'),
+)
 
 
 class Habit(models.Model):
@@ -9,46 +13,23 @@ class Habit(models.Model):
     Модель представляет собой привычку пользователя.
     Содержит информацию о владельце, времени, месте, частоте выполнения и других характеристиках привычки.
     """
-    owner = models.ForeignKey(
-        User, on_delete=models.CASCADE, verbose_name="Пользователь"
-    )
-    location = models.CharField(
-        max_length=255,
-        verbose_name="Локация",
-        help_text="Укажите локацию, в которой необходимо выполнять привычку",
-    )
-    time = models.TimeField(
-        auto_now=False,
-        auto_now_add=False,
-        verbose_name="Время выполнения привычки",
-        help_text="Установите время, когда необходимо выполнять привычку",
-    )
-    action = models.CharField(
-        max_length=100,
-        verbose_name="Активность",
-        help_text="Укажите действие, которое нужно совершить",
-    )
-    estimated_time = models.IntegerField(
-        verbose_name="Время потраченное на выполнение (в секундах)"
-    )
-    periodicity = models.IntegerField(
-        default=1,
-        verbose_name="Периодичность в днях",
-        help_text="Укажите периодичность выполнения привычки в днях (по умолчанию ежедневная)",
-    )
-    nice_habit = models.BooleanField(
-        default=True, verbose_name="Приятная привычка", **NULLABLE
-    )
-    is_public = models.BooleanField(
-        default=False, verbose_name="Опубликована", **NULLABLE
-    )
+
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name="Пользователь")
+    location = models.CharField(max_length=255, verbose_name="Локация")
+    time = models.TimeField(verbose_name="Время выполнения привычки")
+    action = models.CharField(max_length=100, verbose_name="Активность")
+    estimated_time = models.IntegerField(verbose_name="Время потраченное на выполнение (в секундах)")
+    periodicity = models.BooleanField(default=False, choices=PERIOD_CHOICES, verbose_name='Периодичность')
+    nice_habit = models.BooleanField(default=True, verbose_name="Приятная привычка")
+    is_public = models.BooleanField(default=False, verbose_name="Опубликована")
     related = models.ForeignKey(
         "self",
         on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
         verbose_name="Связанная с другой привычкой",
-        **NULLABLE,
     )
-    reward = models.CharField(max_length=100, verbose_name="Вознаграждение", **NULLABLE)
+    reward = models.CharField(max_length=100, null=True, blank=True, verbose_name="Вознаграждение")
 
     def __str__(self):
         return f"Я буду {self.action} в {self.time} в {self.location}"
--- habits/tasks.py
+++ habits/tasks.py
@@ -9,6 +9,12 @@ from telebot import logger
 from habits.models import Habit
 from habits.services import send_telegram_message
 
+# Добавляем список дней недели для удобства работы с ними
+DAYS_OF_WEEK = [
+    'Monday', 'Tuesday', 'Wednesday',
+    'Thursday', 'Friday', 'Saturday', 'Sunday'
+]
+
 
 @shared_task()
 def telegram_notification():
@@ -19,17 +25,24 @@ def telegram_notification():
     current_time = now.time()
     current_time_less = (now - timedelta(minutes=5)).time()
 
-    # Фильтруем привычки по времени
-    habits = Habit.objects.filter(time__range=(current_time_less, current_time))
+    # Получаем текущий день недели
+    today = DAYS_OF_WEEK[now.weekday()]
+
+    # Фильтрация привычек по времени и периодичности
+    habits = Habit.objects.filter(
+        time__gte=current_time_less,
+        time__lte=current_time
+    ).select_related('owner')  # Используем select_related для оптимизации запросов к базе данных
 
     for habit in habits:
-        try:
-            user_tg = habit.owner.tg_chat_id
-            message = f"Я буду {habit.action} в {habit.time} в {habit.location}"
-            send_telegram_message(user_tg, message)
+        if habit.periodicity == True or (habit.periodicity == False and today == habit.day_of_week):
+            try:
+                user_tg = habit.owner.tg_chat_id
+                message = f"Я буду {habit.action} в {habit.time} в {habit.location}"
+                send_telegram_message(user_tg, message)
 
-            # Логируем успешное уведомление
-            logger.info(f"Успешно отправлено уведомление пользователю {habit.owner}: {message}")
-        except Exception as e:
-            # Логируем ошибку
-            logger.error(f"Ошибка при отправке уведомления пользователю {habit.owner}: {e}")
+                # Логируем успешное уведомление
+                logger.info(f"Успешно отправлено уведомление пользователю {habit.owner}: {message}")
+            except Exception as e:
+                # Логируем ошибку
+                logger.error(f"Ошибка при отправке уведомления пользователю {habit.owner}: {e}")
