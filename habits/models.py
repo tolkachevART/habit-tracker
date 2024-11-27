7d8ce6d58cf4acb82a16d53dc7dd8853905007cc
tolkachevART <168184655+tolkachevART@users.noreply.github.com>
2024-11-27 11:44:53 +0300

    remarks_3

--- habits/models.py
+++ habits/models.py
@@ -2,11 +2,6 @@ from django.db import models
 
 from users.models import User
 
-PERIOD_CHOICES = (
-    (True, 'Ежедневная'),
-    (False, 'Еженедельная'),
-)
-
 
 class Habit(models.Model):
     """
@@ -20,7 +15,7 @@ class Habit(models.Model):
     action = models.CharField(max_length=100, verbose_name="Активность")
     estimated_time = models.IntegerField(verbose_name="Время потраченное на выполнение (в секундах)")
     last_performed = models.DateField(null=True, blank=True, verbose_name='Дата последнего выполнения')
-    periodicity = models.BooleanField(default=False, choices=PERIOD_CHOICES, verbose_name='Периодичность')
+    periodicity = models.PositiveIntegerField(default=1, verbose_name='Интервал выполнения (дни)')
     nice_habit = models.BooleanField(default=True, verbose_name="Приятная привычка")
     is_public = models.BooleanField(default=False, verbose_name="Опубликована")
     related = models.ForeignKey(
@@ -37,5 +32,6 @@ class Habit(models.Model):
 
     class Meta:
         verbose_name = "Привычка"
-        verbose_name_plural = "Привычки"
-        ordering = ["-id"]
+
+    verbose_name_plural = "Привычки"
+    ordering = ["-id"]
